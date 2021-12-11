4a5281e5fb46f194aee52ffb8ae5075ba486d451
0xReadMe <87978490+0xReadMe@users.noreply.github.com>
2021-12-11 16:49:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,137 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from Screenshot import Screenshot_Clipping
+from methods.options import set_options
+import time
+import configparser
+import os
+
+#  Work with config.ini
+config = configparser.ConfigParser()
+dirpath = os.getcwd()
+config.read(dirpath + r'\config.ini')
+
+
+login = config.get('SETTINGS', 'LOGIN')
+password = config.get('SETTINGS', 'PASSWORD')
+#  End work with config.ini
+
+#  Start a browser with preset options
+driver = webdriver.Firefox(options=set_options())
+
+
+def extra_page_continue():
+    """Condition if an extra page comes out."""
+    try:
+        #  Find extra page
+        driver.find_element(By.XPATH, '/html/body/div/div[2]/div[4]/a')
+        return True
+    except NoSuchElementException:
+        return False
+
+
+def school_mos_reg():
+    """Make screenshots at schoolmosreg and save it."""
+    print('Попали на главную страницу школьного портала')
+
+    # Take a screenshot of main page and save it
+    obs = Screenshot_Clipping.Screenshot()
+    print('Делаем скриншот...')
+    obs.full_Screenshot(driver, save_path=r'.', image_name='Main.png')  # noqa
+    print('Сохранили скриншот в Main.png')
+
+    # Go to academic performance
+    driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/ul/li[1]/ul/li[3]/a').click()  # noqa
+    print('Нажали на кнопку "Успеваемость" ')
+
+    # Take a screenshot of a work week
+    print('Делаем скриншот...')
+    obs.full_Screenshot(driver, save_path=r'.', image_name='Academic_performance.png')  # noqa
+    print('Скриншот сохранен в Academic_performance.png')
+
+    # Go to marks
+    driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[6]/ul/li[4]/a').click()  # noqa
+    print('Нажали на кнопку "По семестрам"')
+
+    # Take a screenshot of a marks
+    print('Делаем скриншот...')
+    obs.full_Screenshot(driver, save_path=r'.', image_name='All_Marks.png')  # noqa
+    print('Скриншот сохранен в All_Marks.png')
+
+    print('Закончили работу.')
+
+
+def main():
+    """
+    Function for WebDriver.
+    Parse schoolmosreg and get grades by subject.
+    """
+    try:
+        # Get the google.com
+        driver.get('https://www.google.com/')
+        print('Попали в поисковик')
+
+        # Enter a request
+        driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys('школьный портал')  # noqa
+        print('Ввели "школьный портал"')
+
+        # CLick on the button "Find"
+        driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[5]/center/input[1]').click()   # noqa
+        time.sleep(1.5)
+        print('Кликнули на кнопку "Поиск"')
+
+        try:
+            # CLick on the search query
+            driver.find_element(By.XPATH, '/html/body/div[7]/div/div[10]/div[1]/div/div[2]/div[2]/div/div/div[3]/div/div/div[1]/a/div/cite').click()  # noqa
+            time.sleep(1.8)
+            print('Нашли страницу входа в школьный портал')
+        except Exception as exc:
+            print('Возникла непредвиденная ошибка!')
+            print(f'Ошибка: {exc}')
+            print('Пожалуйста, запустите заново программу!')
+
+        # Enter a login at schoolmosreg
+        loginning = driver.find_element(By.NAME, 'login')
+        loginning.send_keys(login)
+        print('Ввели логин')
+
+        # Enter a password at schoolmosreg
+        passwording = driver.find_element(By.NAME, 'password')
+        passwording.send_keys(password)
+        print('Ввели пароль')
+
+        # Click on button 'Enter'
+        enter = driver.find_element(By.XPATH, '/html/body/div/div/div/form/input[3]')  # noqa
+        enter.click()
+        time.sleep(2)
+        print('Нажали на кнопку "Войти"')
+
+        #  Check if extra page is exist
+        if extra_page_continue() is True:
+            print('Попали на лишнюю страницу, производим перенаправление')
+
+            #  Redirect to main page
+            continues = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[4]/a')  # noqa
+            continues.click()
+
+            school_mos_reg()
+
+        elif extra_page_continue() is False:
+            school_mos_reg()
+        else:
+            print('Something went wrong')
+
+    except Exception as exc:
+        print(exc)
+    finally:
+        #  Exit the browser
+        driver.close()
+        driver.quit()
+        print('Закрыли браузер.')
+
+        os.remove(dirpath + r'\geckodriver.log')
+
+if __name__ == '__main__':
+    main()
+    a = input()
---
+++ methods/options.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+
+def set_options():
+    """
+    Set options for WebDriver.
+    Enable option.headless, if you need.
+    """
+    option = webdriver.FirefoxOptions()
+    option.set_preference('dom.webdriver.enabled', False)
+    option.set_preference('dom.webnotifications.enabled', False)
+    option.set_preference('media.volume_scale', '0.0')
+    option.headless = True
+    return option
